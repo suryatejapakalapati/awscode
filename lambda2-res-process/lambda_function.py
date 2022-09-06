a9014a1e5a140714c5db3af4b6bc79ea2d7cd62d
suryatejapakalapati <57919273+suryatejapakalapati@users.noreply.github.com>
2022-09-06 11:27:47 -0400

    Create lambda_function.py

---
+++ lambda2-res-process/lambda_function.py
@@ -0,0 +1,40 @@
+import os
+import json
+import logging
+from helper import process_response, process_error
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def lambda_handler(event, context):
+
+    try:
+        BUCKET_NAME = os.environ["BUCKET_NAME"]
+        logger.info(f"Destination bucket: {BUCKET_NAME}")
+    except Exception as e:
+        error_msg = process_error()
+        logger.error(error_msg)
+
+    try:
+        if "Records" in event:
+            logger.info(f"Event: {event}")
+            job_id = json.loads(event["Records"][0]["Sns"]["Message"])["JobId"]
+            file_name_a = json.loads(event["Records"][0]["Sns"]["Message"])['DocumentLocation']['S3ObjectName']
+            #print("job_id")
+            #print(job_id)
+            logger.info(f"JobId: {job_id}")
+
+            logger.info("Parsing initiated")
+            process_response(
+                BUCKET_NAME, job_id,file_name_a, get_table=False, get_kv=True, get_text=False
+            )
+
+            return {
+                "statusCode": 200,
+                "body": json.dumps("File uploaded successfully!"),
+            }
+    except Exception as e:
+        error_msg = process_error()
+        logger.error(error_msg)
+        return {"statusCode": 500, "body": json.dumps("Error parsing the response!")}
